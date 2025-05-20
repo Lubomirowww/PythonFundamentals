a913cc57641fa5c0d0411a12ad75b911cf4929da
Dimitar Dimitrov <stu2401261059@uni-plovdiv.bg>
2025-05-20 17:13:19 +0300

    20.05.2025

--- Regular_Expressions/task1_Match_Full_Name.py
+++ Regular_Expressions/task1_Match_Full_Name.py
@@ -1,7 +1,7 @@
 import re
 
-text = input()\
+text = input()
 
-matches = re.findall(r"\b[A-Z][a-z]+ [A-Z][a-z]+\b", text)
+matches = re.findall(r"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b", text)
 
 print(" ".join(matches))
--- Regular_Expressions/task2_Match_Phone_Number.py
+++ Regular_Expressions/task2_Match_Phone_Number.py
@@ -6,4 +6,3 @@ matches = re.findall(r"\+359 2 \d{3} \d{4}|\+359-2-\d{3}-\d{4}", number)
 
 print(" ".join(matches))
 
-#maybe not work true
