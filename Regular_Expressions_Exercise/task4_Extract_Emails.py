80a6ee5a41e000e81ffb08971cf572936b9d9cdc
Dimitar Dimitrov <stu2401261059@uni-plovdiv.bg>
2025-05-21 22:26:28 +0300

    21.05.2025v2

---
+++ Regular_Expressions_Exercise/task4_Extract_Emails.py
@@ -0,0 +1,16 @@
+import re
+
+text = input()
+
+user_pattern = r'( |^)[a-zA-Z0-9]+((\.|\-|_)[a-zA-Z0-9]+)*'
+host_pattern = r'[a-zA-Z]+(-[a-zA-Z]+)*(\.[a-zA-Z]+(-[a-zA-Z]+)*)'
+
+pattern =rf'{user_pattern}@{host_pattern}'
+
+emails=re.finditer(pattern, text)
+
+for email in emails:
+    print(email[0])
+
+
+#Примерен код- Please contact us at: support@github.com.
