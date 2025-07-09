c1a8621f4c4b62462a25b200bc8b45f5bd55708f
Dimitar Dimitrov <stu2401261059@uni-plovdiv.bg>
2025-07-09 20:01:38 +0300

    fun ex

---
+++ mini ex/task1.py
@@ -0,0 +1,41 @@
+car_model = input()
+car_color = input()
+price = 0
+features = []
+
+
+
+available_features = {
+
+    "navigation": 1500,
+    "leather seats": 2000,
+    "panorama": 1000,
+}
+
+user_input = input("Въведи избраните екстри:")
+
+
+equipment_features = [f.strip().lower() for f in user_input.split(", ")]
+
+for feature in equipment_features:
+    if feature in available_features:
+        features.append(feature)
+        price += available_features[feature]
+    else:
+        print(f"{feature} не е валидна екстра и ще бъде пропусната.")
+
+
+if car_model == "Sedan":
+    price += 20000
+
+elif car_model == "SUV":
+    price += 30000
+
+elif car_model == "Coupe":
+    price += 25000
+
+print(f"\nКолата е {car_model} в цвят {car_color}.")
+print(f"Избрани екстри: {', '.join(features)}")
+print(f"Крайна цена: {price} лв")
+
+
